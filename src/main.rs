c51e8f3ac8cf419eb8289f02c5b2400a2d660b2d
Huon Wilson <huon@exoflare.io>
2023-09-23 13:34:56 +1000

    minimal

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use futures::stream::StreamExt;
+use tokio::io::AsyncWriteExt;
 
 #[tokio::main]
 async fn main() {
@@ -7,36 +7,11 @@ async fn main() {
 
   // unique for every run to test behaviour:
   let now = std::time::SystemTime::now().duration_since(std::time::SystemTime::UNIX_EPOCH).unwrap();
-  let make_key = |s: &str| dbg!(format!("{}-{}", s, now.as_secs_f64()));
 
-  println!("overwrite");
-  let key_rewrite = make_key("rewrite");
-  op.write(&key_rewrite, "data").await.unwrap();
-  dbg!(op.read(&key_rewrite).await.unwrap().len());
+  let key = format!("key-{}", now.as_secs_f64());
+  let mut writer = op.writer(&key).await.unwrap();
 
-  // write again
-  let _ = dbg!(op.write(&key_rewrite, "data").await);
-  let _ = dbg!(op.writer(&key_rewrite).await.map(|_| ()));
-
-  println!("from file via tokio::io::copy");
-  let key_file_tokio = make_key("file-tokioiocopy");
-  let big_data = "data".repeat(10000);
-  let big_data = big_data.as_bytes();
-  let mut reader = std::io::Cursor::new(big_data);
-  let mut writer = op.writer(&key_file_tokio).await.unwrap();
-
-  dbg!(tokio::io::copy(&mut reader, &mut writer).await).unwrap();
-  dbg!(writer.close().await).unwrap();
-  // what was written?
-  dbg!(op.read(&key_file_tokio).await.unwrap().len());
-
-  println!("from file via sink");
-  let key_file_writer = make_key("file-writersink");
-  let stream = futures::stream::repeat_with(|| Ok(bytes::Bytes::from("data"))).take(10);
-  let mut writer = op.writer(&key_file_writer).await.unwrap();
-
-  dbg!(writer.sink(stream).await).unwrap();
-  dbg!(writer.close().await).unwrap();
-  // what was written?
-  dbg!(op.read(&key_file_writer).await.unwrap().len());
+  writer.write_all(&[0]).await.unwrap();
+  writer.write_all(&[1]).await.unwrap();
+  writer.close().await.unwrap();
 }
